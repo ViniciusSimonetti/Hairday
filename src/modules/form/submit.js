81432db01fed434f84e708d29f0938bc1343fe3c
ViniciusSimonetti <viniciussimonetti25@gmail.com>
2026-02-11 18:00:50 -0300

    Setting the current date

---
+++ src/modules/form/submit.js
@@ -0,0 +1,22 @@
+import dayjs from "dayjs"
+
+const form = document.querySelector('form') 
+const selectDate = document.getElementById('date')
+
+
+// Data Atual para o input
+const inputToday = dayjs(new Date()).format('YYYY-MM-DD')
+
+// Carrega a data atual no campo de data e define a data mínima para a data atual, evitando que o usuário selecione datas passadas
+selectDate.value = inputToday
+selectDate.min = inputToday
+
+
+// Adiciona um ouvinte de evento para o envio do formulário
+form.onsubmit = (event) => {
+// Evita o comportamento padrão de recarregar a página
+  event.preventDefault() 
+
+    console.log('Formulário enviado!')
+
+}
