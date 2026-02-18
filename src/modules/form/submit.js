0a0afef10d6aa4461f30ac7f86790d480d8f50f6
ViniciusSimonetti <viniciussimonetti25@gmail.com>
2026-02-18 20:19:56 -0300

    Refactorant and Setting the Current Date

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,7 +1,21 @@
+import dayjs from "dayjs" 
+
 
 const form = document.querySelector('form');
+const selectDate = document.getElementById('date');
+
+//data atual para o input date
+const inputToday = dayjs(new Date()).format('YYYY-MM-DD');
+
+// Carrega a data atual 
+selectDate.value = inputToday;
+
+//define a data minima como sendo a atual para evitar que o usuário selecione uma data passada
+selectDate.min = inputToday;
+
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
+  console.log('Formulário enviado!');
 })
