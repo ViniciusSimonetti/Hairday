93c89c3d79feec5efa857b4202354555aa38184e
ViniciusSimonetti <viniciussimonetti25@gmail.com>
2026-02-19 19:15:38 -0300

    Sending a selected time to the API

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,7 +1,9 @@
 import dayjs from "dayjs" 
 
 const form = document.querySelector('form');
+const clientName = document.getElementById('client');
 const selectDate = document.getElementById('date');
+
 
 //data atual para o input date
 const inputToday = dayjs(new Date()).format('YYYY-MM-DD');
@@ -14,5 +16,41 @@ selectDate.min = inputToday;
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  console.log('Formulário enviado!');
+  try {
+    //recuperando o nome do cliente 
+    const name = clientName.value.trim();
+    
+    if (!name) {
+    return alert("Informe o nome do cliente.");
+    }
+    
+    //recuperando o horario selecionado
+    const hourSelected = document.querySelector(".hour-selected")
+    
+    //validando se o cliente selecionou um horário
+    if (!hourSelected) {
+      return alert("Selecione um horário para o agendamento.");
+    }
+
+    // Recuperar somente a hora 
+    const [hour] = hourSelected.innerText.split(':');
+
+    //Inserir a hora na data 
+    const when= dayjs(selectDate.value).add(hour, 'hour')
+   
+    //Gera um ID aleatório para o agendamento
+    const id = new Date().getTime();
+
+    console.log({
+      id,
+      name,
+      when: when.format('DD/MM/YYYY HH:mm')
+    })
+
+  } catch (error) {
+    alert('Ocorreu um erro ao enviar o formulário. Por favor, tente novamente.');
+    console.log(error);
+  }
+
+
 })
