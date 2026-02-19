7f7a9750929350d547b21249b4a8b2cbe1e9e022
ViniciusSimonetti <viniciussimonetti25@gmail.com>
2026-02-19 10:14:15 -0300

    Content loading event

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,16 +1,13 @@
 import dayjs from "dayjs" 
 
-
 const form = document.querySelector('form');
 const selectDate = document.getElementById('date');
 
 //data atual para o input date
 const inputToday = dayjs(new Date()).format('YYYY-MM-DD');
 
-// Carrega a data atual 
+// Carrega a data atual e define a data minima como sendo a atual para evitar que o usuário selecione uma data passada
 selectDate.value = inputToday;
-
-//define a data minima como sendo a atual para evitar que o usuário selecione uma data passada
 selectDate.min = inputToday;
 
 
